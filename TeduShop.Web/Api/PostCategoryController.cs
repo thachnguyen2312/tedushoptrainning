58229f116bafa1bcf6ab995000309d663bf334c0
DESKTOP-I3ABAG6\MY <thachnguyen2312@gmail.com>
2018-01-02 01:10:58 +0700

    add dependency base on autofac and owin.systemweb

--- TeduShop.Web/Api/PostCategoryController.cs
+++ TeduShop.Web/Api/PostCategoryController.cs
@@ -21,17 +21,17 @@ namespace TeduShop.Web.Api
         {
             return CreateHttpResponse(request, () =>
             {
-                HttpResponseMessage response = null;
-                if (ModelState.IsValid)
-                {
-                    request.CreateErrorResponse(HttpStatusCode.BadRequest, ModelState);
-                }
-                else
-                {
+                //HttpResponseMessage response = null;
+                //if (ModelState.IsValid)
+                //{
+                //    request.CreateErrorResponse(HttpStatusCode.BadRequest, ModelState);
+                //}
+                //else
+                //{
                     var listCategory = _postCategoryService.GetAll();
-                    _postCategoryService.Save();
-                    response = request.CreateResponse(HttpStatusCode.OK);
-                }
+                //_postCategoryService.Save();
+                HttpResponseMessage response = request.CreateResponse(HttpStatusCode.OK, listCategory);
+                //}
                 return response;
             });
         }
