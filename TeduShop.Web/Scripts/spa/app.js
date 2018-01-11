277289c2cb253fa7ec6b68409a9c3cc96b77de47
DESKTOP-I3ABAG6\MY <thachnguyen2312@gmail.com>
2018-01-11 23:07:24 +0700

    17_20_service-directive-controller-app

---
+++ TeduShop.Web/Scripts/spa/app.js
@@ -0,0 +1,34 @@
+﻿var myApp = angular.module('myModule', []);
+
+myApp.controller("myController", myController);
+myApp.directive("myDirective", myDirective);
+myApp.service("validatorService", validatorService);
+myController.$inject = ['$scope','validatorService'];
+
+function myController($scope, validatorService) {
+    //$scope.message = "This is message from Controller";
+    $scope.message = validatorService.checkNumberFun(9);
+}
+
+function validatorService($window) {
+    return {
+        checkNumberFun: checkNumber
+    }
+    function checkNumber(number) {
+        if (number % 2 == 0) {
+            return 'this is even';
+        }
+        else {
+            return 'this is odd';
+        }
+    }
+}
+
+function myDirective() {
+    return {
+        templateUrl: "/Scripts/spa/myDirective.html"
+    }
+}
+//myApp.controller("myController", function ($scope) {
+//    $scope.message = "This is message from Controller";
+//});
